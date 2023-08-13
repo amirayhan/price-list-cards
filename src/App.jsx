548afb0f52380ffdedc42bf801652f8c7c8bb8f6
amirayhan <jaber.ac1983@gmail.com>
2023-08-13 10:29:03 +0600

    install axios and use it, also make a TinyBarCharts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Navbar from "./components/Navbar/Navbar";
+import PhoneBar from "./components/PhoneBar/PhoneBar";
 import PriceList from "./components/PriceList/PriceList";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             </div>
             <PriceList></PriceList>
             <Dashboard></Dashboard>
+            <PhoneBar></PhoneBar>
         </>
     );
 }
